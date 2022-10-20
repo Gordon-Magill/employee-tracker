05cfc218eb1904c69516b7cb07e8d50d425c17d4
Gordon-Magill <gordon.magill@gmail.com>
2022-10-19 23:37:43 -0700

    Added template routes and finalized v1 seed.sql

--- db/seed.sql
+++ db/seed.sql
@@ -1,16 +1,17 @@
 USE company_db;
 
-INSERT INTO department (name)
+INSERT INTO departments (deptName)
 VALUES ('Cell Culture'),
 ('Purification'),
 ('ADQC');
 
-INSERT INTO role (title, salary, department_id)
-VALUES ('Principal Engineer', 150000.00, 1),
-('Senior Engineer', 100000.00, 2),
-('Research associate', 80000.00, 3);
+INSERT INTO roles (title, salary, department_id)
+VALUES ('CC Principal Engineer', 150000.00, 1),
+('CC Senior Engineer', 125000.00,1),
+('PurDev Senior Engineer', 100000.00, 2),
+('ADQC Research associate', 70000.00, 3);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES ('mr firstname','lastname',1, NULL),
-('intern firstname','intern lastname',3,1),
+('intern firstname','intern lastname',4,1),
 ('engineer firstname', 'engineer lastname',2,1);
