522424c4c7511ef1f69f1f2ca8059ae279ac1c94
Gordon-Magill <gordon.magill@gmail.com>
2022-10-26 05:05:53 -0700

    Improved comments

--- index.js
+++ index.js
@@ -3,14 +3,11 @@ require("dotenv").config();
 
 // Import the three major custom classes
 const Employees = require("./lib/Employees");
-const Roles = require('./lib/Roles')
-const Departments = require('./lib/Departments')
+const Roles = require("./lib/Roles");
+const Departments = require("./lib/Departments");
 
 // Helper functions for menu operation and database closeout
-const {
-  getMenuOption,
-  closeDB
-} = require("./src/helperFunctions");
+const { getMenuOption, closeDB } = require("./src/helperFunctions");
 
 // Initializing global objects
 let EMPLOYEES = new Employees();
@@ -19,21 +16,19 @@ let ROLES = new Roles();
 
 // Continuously loops through options until the user elects to exit the program
 async function cycleMenuOptions() {
-
   // At the beginning of the loop, ask the user to select an option
   let option = await getMenuOption();
 
   // Take action based on the user input
   switch (option) {
-
     // Special case to close the DB connection at the end and terminate the menu loop
     case "Quit":
       closeDB();
-      console.log('\nGoodbye.\n')
+      console.log("\nGoodbye.\n");
       return;
 
     case "View all departments":
-      DEPARTMENTS.showTable()
+      DEPARTMENTS.showTable();
       cycleMenuOptions();
       break;
 
@@ -48,10 +43,10 @@ async function cycleMenuOptions() {
       break;
 
     case "Add department":
-      await DEPARTMENTS.addNewDept()
+      await DEPARTMENTS.addNewDept();
 
       // Keeping DEPARTMENTS up to date with db content
-      DEPARTMENTS.refreshDepartmentList()
+      DEPARTMENTS.refreshDepartmentList();
 
       console.log("\nSUCCESS:\nSuccessfully wrote new deptartment to db.\n");
       cycleMenuOptions();
@@ -62,7 +57,7 @@ async function cycleMenuOptions() {
       await ROLES.addNewRole(DEPARTMENTS.getDepartmentList());
 
       // Keeping ROLES up to date with db content
-      ROLES.refreshRoleList()
+      ROLES.refreshRoleList();
 
       console.log("\nSUCCESS:\nSuccessfully wrote new role to db.\n");
       cycleMenuOptions();
@@ -70,34 +65,31 @@ async function cycleMenuOptions() {
 
     case "Add employee":
       // Create new employee that can draw from existing roles
-      await EMPLOYEES.addNewEmployee(ROLES.getRoleList())
+      await EMPLOYEES.addNewEmployee(ROLES.getRoleList());
 
       // Keeping EMPLOYEES up to date with db content
-      EMPLOYEES.refreshEmployeeList()
-      
+      EMPLOYEES.refreshEmployeeList();
+
       console.log("\nSUCCESS:\nSuccessfully wrote new employee to db.\n");
       cycleMenuOptions();
       break;
 
     case "Change employee role":
       // Update employee role based on available roles
-      await EMPLOYEES.changeEmployeeRole(ROLES.getRoleList())
+      await EMPLOYEES.changeEmployeeRole(ROLES.getRoleList());
 
       // Keeping EMPLOYEES up to date with db content
-      EMPLOYEES.refreshEmployeeList()
+      EMPLOYEES.refreshEmployeeList();
 
       console.log(
         "\nSUCCESS:\nSuccessfully wrote modified employee info to db.\n"
       );
       cycleMenuOptions();
       break;
-  };
-};
-
-
-
+  }
+}
 
 // Start the program with some nice text introduction
-console.clear()
-console.log(`\nWelcome to the company CMS\n`)
+console.clear();
+console.log(`\nWelcome to the company CMS\n`);
 cycleMenuOptions();
