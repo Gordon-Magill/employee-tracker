a21068bc3df98f0dc53e9d5bd891e1af88cac603
Gordon-Magill <gordon.magill@gmail.com>
2022-10-26 03:01:43 -0700

    Added placeholder class definition files + added README + commenting code

---
+++ README.md
@@ -0,0 +1,42 @@
+# Employee Tracker CMS
+[![License: Do What the Fuck You Want to Public License](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)
+
+## Description
+
+A small demo CMS (content management system) for tracking company department, role, and employee information with a simple terminal interface. View information on the company, add new elements, and change  information as desired
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [Tests](#tests)
+- [Contributing](#contributing)
+- [Questions](#questions)
+- [License](#license)
+
+## Installation
+
+Install and setup mysql locally. Run "npm i" in the repo directory. For demonstration purposes also load the schema and seed (in the 'db' directory).
+
+## Usage
+
+Run "node index" in the repo directory. Follow text prompts.
+
+## Tests
+
+No tests implemented at this time.
+
+## Contributing
+
+Not accepting contributions at this time, but feel free to fork and modify freely.
+
+## Questions
+
+[Github: Gordon-Magill](https://github.com/Gordon-Magill)<br>
+Email: gordon.magill@gmail.com
+Please reach out via provided email for any questions.
+
+## License
+
+Licensed under [Do What the Fuck You Want to Public License](http://www.wtfpl.net/about/).
--- index.js
+++ index.js
@@ -14,51 +14,60 @@ const {
   addNewEmployee,
   getMenuOption,
   changeEmployeeRole,
-  db,
   closeDB,
 } = require("./src/helperFunctions");
 
 // Continuously loops through options until the user elects to exit the program
 async function cycleMenuOptions() {
+
+  // At the beginning of the loop, ask the user to select an option
   let option = await getMenuOption();
 
+  // Take action based on the user input
   switch (option) {
+
+    // Special case to close the DB connection at the end and terminate the menu loop
     case "Quit":
       closeDB();
       console.log('\nGoodbye.\n')
       return;
+
     case "View all departments":
       const depts = await getDepts();
       showDepts(depts[0]);
       cycleMenuOptions();
       break;
+
     case "View all roles":
       let roles = await getRoles();
       showRoles(roles[0]);
       cycleMenuOptions();
       break;
+
     case "View all employees":
       let employees = await getEmployees();
       showEmployees(employees[0]);
-
       cycleMenuOptions();
-
       break;
+
     case "Add department":
       await addNewDept();
       console.log("\nSUCCESS:\nSuccessfully wrote new deptartment to db.\n");
       cycleMenuOptions();
       break;
+
     case "Add role":
       await addNewRole();
       console.log("\nSUCCESS:\nSuccessfully wrote new role to db.\n");
       cycleMenuOptions();
       break;
+
     case "Add employee":
       await addNewEmployee();
       console.log("\nSUCCESS:\nSuccessfully wrote new employee to db.\n");
       cycleMenuOptions();
       break;
+
     case "Change employee role":
       await changeEmployeeRole();
       console.log(
@@ -66,9 +75,8 @@ async function cycleMenuOptions() {
       );
       cycleMenuOptions();
       break;
-  }
-}
+  };
+};
 
 // Start the program
 cycleMenuOptions();
-
